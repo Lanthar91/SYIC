a584f4415e07eb48b16e2e4730bc3ed062e2b0e4
Lanthar91 <elkhan.aliyev.91@gmail.com>
2025-03-22 12:34:01 +0400

    Update m.js

--- m.js
+++ m.js
@@ -36,7 +36,7 @@
           document.head.appendChild(script);
         });
 
-        loadScript('https://<твой-ник>.github.io/sycbox/jsnes.js').then(() => {
+        loadScript('https://<lanthar91.github.io/SYIC/jsnes.js').then(() => {
           const nes = new jsnes.NES({
             onFrame(frameBuffer) {
               const imageData = context.getImageData(0, 0, 256, 240);
@@ -51,8 +51,8 @@
           });
 
           const files = [
-            { title: 'Super Mario Bros.', url: 'https://<твой-хостинг>/mario.nes' },
-            { title: 'Contra', url: 'https://<твой-хостинг>/contra.nes' },
+            { title: 'Super Mario Bros.', url: 'https://lanthar91.github.io/roms/mario.nes' },
+            { title: 'Contra', url: 'https://lanthar91.github.io/roms/contra.nes' },
           ];
 
           const explorer = new Lampa.Explorer({
